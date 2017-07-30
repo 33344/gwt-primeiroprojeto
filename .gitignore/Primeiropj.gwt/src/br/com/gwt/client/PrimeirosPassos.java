f3993ddf827614142741f41f7074ef07e2a491ba
Anderson <Anderson.rocha.sousa@outlook.com>
2017-07-30 19:26:54 -0300

    Add files via upload
    inicio do projeto

---
+++ .gitignore/Primeiropj.gwt/src/br/com/gwt/client/PrimeirosPassos.java
@@ -0,0 +1,21 @@
+package br.com.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.sun.java.swing.plaf.windows.resources.windows;
+
+/**
+ * 
+ * @author Ragnar
+ * aula sobre gwt anderson
+ *
+ */
+
+public class PrimeirosPassos implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("Bom curso");
+	}
+
+}
